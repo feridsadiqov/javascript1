040eef2fe01b0cb47d1b4110a778b99942dec7bb
Ferid <feridsadiqov38@gmail.com>
2023-07-17 10:33:08 +0400

    commit

---
+++ index.js
@@ -0,0 +1,68 @@
+// let a = prompt("Hello World")
+
+// console.log(a); 
+
+// let b = prompt("Do you want next ?")
+
+// console.log(b); 
+
+// let num = prompt("password")
+
+// console.log(num); 
+
+// 2
+
+
+// let a=prompt("enter first number")
+
+// let b=prompt("enter secund number")
+
+// let sum=parseInt(a)+parseInt(b)
+
+// alert(sum);
+
+
+// 3
+
+// let number;
+// do{
+//     number = parseInt( prompt("Enter a number greater than 100",""))
+// } while(number <= 100 || isNaN(number));
+
+
+
+
+// const num = parseInt(prompt("enter number"))
+
+// if(num > 0 || num < 0){
+//     console.log(1)
+// }else{
+//     console.log(0)
+// }
+
+
+// const login= prompt("Enter a login")
+// console.log(login);
+
+// if (login=='Employe') {
+
+//     alert ("Empolyee==Hello")
+// }
+// else if(login=='Director'){
+
+//     alert("Director==Greetings")
+// }
+// else{
+//     alert('error')
+// }
+
+const x=prompt("x")
+const y=prompt("y")
+alert (x**y)
+
+
+
+ 
+
+
+
